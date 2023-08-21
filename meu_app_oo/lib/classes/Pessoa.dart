328a1f130860643c8f4b5da298f21ef922a64057
julianasalafia <juliana.salafia1@gmail.com>
2023-08-21 14:45:33 -0300

    OO: encapsulamento

---
+++ meu_app_oo/lib/classes/Pessoa.dart
@@ -0,0 +1,20 @@
+class Pessoa {
+  String? _nome;
+  String? _endereco;
+
+  void setNome(String nome) {
+    _nome = nome;
+  }
+
+  void setEndereco(String endereco) {
+    _endereco = endereco;
+  }
+
+  String? getNome() {
+    return _nome!.toUpperCase();
+  }
+
+  String? getEndereco() {
+    return _endereco;
+  }
+}
