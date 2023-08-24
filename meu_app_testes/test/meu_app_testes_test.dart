48073c8104c79127537736129070714e1f52b1d3
julianasalafia <juliana.salafia1@gmail.com>
2023-08-24 11:16:52 -0300

    testes: introduçao

---
+++ meu_app_testes/test/meu_app_testes_test.dart
@@ -0,0 +1,12 @@
+import 'package:meu_app_testes/meu_app_testes.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+
+  test('calculate1', () {
+    expect(calculate(), 41);
+  });
+}
