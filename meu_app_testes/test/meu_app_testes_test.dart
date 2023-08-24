0bb27f7b7c507b89cb358a6981f1bc46de7b5c6e
julianasalafia <juliana.salafia1@gmail.com>
2023-08-24 12:46:44 -0300

    testes: grupo de testes

--- meu_app_testes/test/meu_app_testes_test.dart
+++ meu_app_testes/test/meu_app_testes_test.dart
@@ -21,4 +21,40 @@ void main() {
     expect(() => app.calcularDesconto(0, 150, false),
         throwsA(TypeMatcher<ArgumentError>()));
   });
+
+  group('calcula o desconto do produto com desconto', () {
+    var valuesToTest = {
+      {'valor': 1000, 'desconto': 150, 'percentual': false}: 850,
+      {'valor': 1000, 'desconto': 15, 'percentual': true}: 850,
+    };
+    valuesToTest.forEach((values, expected) {
+      test('Entrada: $values Esperado: $expected', () {
+        expect(
+            app.calcularDesconto(
+                double.parse(values['valor'].toString()),
+                double.parse(values['desconto'].toString()),
+                values['percentual'] == true),
+            equals(expected));
+      });
+    });
+  });
+
+  group(
+      'calcula o desconto do produto informando valores zerados, deve gerar erro',
+      () {
+    var valuesToTest = {
+      {'valor': 0, 'desconto': 150, 'percentual': false},
+      {'valor': 1000, 'desconto': 0, 'percentual': true},
+    };
+    for (var values in valuesToTest) {
+      test('Entrada: $values', () {
+        expect(
+            () => app.calcularDesconto(
+                double.parse(values['valor'].toString()),
+                double.parse(values['desconto'].toString()),
+                values['percentual'] == true),
+            throwsA(TypeMatcher<ArgumentError>()));
+      });
+    }
+  });
 }
