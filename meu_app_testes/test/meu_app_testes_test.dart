5924f96ae1a32dee43740cd874af56e0b3f6f652
julianasalafia <juliana.salafia1@gmail.com>
2023-08-24 12:18:50 -0300

    testes: testes que esperam erros

--- meu_app_testes/test/meu_app_testes_test.dart
+++ meu_app_testes/test/meu_app_testes_test.dart
@@ -3,10 +3,22 @@ import 'package:test/test.dart';
 
 void main() {
   test('calcula o desconto do produto com desconto sem porcentagem', () {
-    expect(app.calcularDesconto(1000, 150, false), 850);
+    expect(app.calcularDesconto(1000, 150, false), equals(850));
+  });
+
+  test(
+      'calcula o desconto do produto com desconto sem porcentagem passando valor do produto zerado',
+      () {
+    expect(() => app.calcularDesconto(0, 150, false),
+        throwsA(TypeMatcher<ArgumentError>()));
   });
 
   test('calcula o desconto do produto com desconto com porcentagem', () {
-    expect(app.calcularDesconto(1000, 15, true), 850);
+    expect(app.calcularDesconto(1000, 15, true), equals(850));
+  });
+
+  test('calcula o desconto do produto com desconto zerado com porcentagem', () {
+    expect(() => app.calcularDesconto(0, 150, false),
+        throwsA(TypeMatcher<ArgumentError>()));
   });
 }
