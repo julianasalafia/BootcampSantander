a2f433c8f4b63fb8d7e2c2b6c53abb7e8bbfd6aa
julianasalafia <juliana.salafia1@gmail.com>
2023-08-29 11:38:18 -0300

    reorganiza pastas

---
+++ 1_modulo/meu_app/bin/meu_app.dart
@@ -0,0 +1,16 @@
+import 'package:meu_app/meu_app.dart' as meu_app;
+
+void main(List<String> arguments) {
+
+  
+
+  var prova1 = 9;
+  var prova2 = 5;
+
+  var media = (prova1 + prova2) / 2;
+
+  var resultado = (prova1 >= 7) ? "o aluno passou" : "o aluno reprovou"; 
+
+
+  print(resultado);
+}
--- meu_app/bin/meu_app.dart
+++
@@ -1,5 +0,0 @@
-import 'package:meu_app/meu_app.dart' as meu_app;
-
-void main(List<String> arguments) {
-  print('Hello world: ${meu_app.calculate()}!');
-}
