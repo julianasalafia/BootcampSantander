5fb920e5dfa8a872f280f731b451a336883e6e1b
julianasalafia <juliana.salafia1@gmail.com>
2023-08-30 13:00:04 -0300

    feat: iniciando a tela de login

--- 2_modulo/trilhaapp/lib/pages/login_page.dart
+++ 2_modulo/trilhaapp/lib/pages/login_page.dart
@@ -12,19 +12,41 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.black54,
         body: SizedBox(
           width: double.infinity,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const SizedBox(height: 70),
-              const Icon(
-                Icons.person,
-                size: 150,
-                color: Colors.blueAccent,
-              ),
               const SizedBox(height: 50),
+              Row(
+                children: [
+                  Expanded(child: Container()),
+                  Expanded(
+                    flex: 8,
+                    child: Image.network(
+                        'https://hermes.digitalinnovation.one/assets/diome/logo.png'),
+                  ),
+                  Expanded(child: Container()),
+                ],
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Já tem cadastro?',
+                style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white),
+              ),
+              SizedBox(height: 10),
+              const Text(
+                'Faça seu login e make the change._',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.white70,
+                ),
+              ),
+              SizedBox(height: 40),
               Container(
                 alignment: Alignment.center,
                 margin: EdgeInsets.symmetric(horizontal: 30),
@@ -34,11 +56,21 @@ class _LoginPageState extends State<LoginPage> {
                   children: [
                     Expanded(
                       flex: 2,
-                      child: Text('Informe seu email:'),
+                      child: Text(
+                        'Informe seu email:',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                     Expanded(
                       flex: 3,
-                      child: Text('email@email.com'),
+                      child: Text(
+                        'email@email.com',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                   ],
                 ),
@@ -48,20 +80,30 @@ class _LoginPageState extends State<LoginPage> {
                 margin: EdgeInsets.symmetric(horizontal: 30),
                 height: 30,
                 width: double.infinity,
-                child: Row(
+                child: const Row(
                   children: [
                     Expanded(
                       flex: 2,
-                      child: Text('Informe a senha:'),
+                      child: Text(
+                        'Informe a senha:',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                     Expanded(
                       flex: 3,
-                      child: Text('**********'),
+                      child: Text(
+                        '**********',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                   ],
                 ),
               ),
-              Expanded(child: Container()),
+              const SizedBox(height: 30),
               Container(
                 alignment: Alignment.center,
                 margin: EdgeInsets.symmetric(horizontal: 30),
@@ -70,16 +112,36 @@ class _LoginPageState extends State<LoginPage> {
                 width: double.infinity,
                 child: Text('Login'),
               ),
-              const SizedBox(height: 20),
+              Expanded(child: Container()),
               Container(
                 alignment: Alignment.center,
                 margin: EdgeInsets.symmetric(horizontal: 30),
-                color: Colors.green,
                 height: 30,
                 width: double.infinity,
-                child: Text('Cadastro'),
+                child: Text(
+                  'Esqueci minha senha',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.yellow,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
               ),
-              const SizedBox(height: 20),
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.symmetric(horizontal: 30),
+                height: 30,
+                width: double.infinity,
+                child: Text(
+                  'Criar conta',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.green,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 60),
             ],
           ),
         ),
