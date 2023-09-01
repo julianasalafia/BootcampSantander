50fc1fd6783a65e485a1ed22678deea7c90eeb39
julianasalafia <juliana.salafia1@gmail.com>
2023-09-01 14:15:48 -0300

    feat: showModalBottomSheet

--- 2_modulo/trilhaapp/lib/shared/widgets/custom_drawer.dart
+++ 2_modulo/trilhaapp/lib/shared/widgets/custom_drawer.dart
@@ -11,15 +11,44 @@ class CustomDrawer extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          UserAccountsDrawerHeader(
-            decoration: BoxDecoration(color: Colors.black54),
-            currentAccountPicture: CircleAvatar(
-              backgroundColor: Colors.white,
-              child: Image.network(
-                  'https://hermes.digitalinnovation.one/assets/diome/logo.png'),
+          InkWell(
+            onTap: () {
+              showModalBottomSheet(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  context: context,
+                  builder: (BuildContext bc) {
+                    return Wrap(
+                      children: [
+                        ListTile(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          title: Text('camera'),
+                          leading: Icon(Icons.camera),
+                        ),
+                        ListTile(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          title: Text('galeria'),
+                          leading: Icon(Icons.photo_album),
+                        ),
+                      ],
+                    );
+                  });
+            },
+            child: UserAccountsDrawerHeader(
+              decoration: BoxDecoration(color: Colors.black54),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Image.network(
+                    'https://hermes.digitalinnovation.one/assets/diome/logo.png'),
+              ),
+              accountName: Text('Juliana Salafia'),
+              accountEmail: Text('ju@gmail.com'),
             ),
-            accountName: Text('Juliana Salafia'),
-            accountEmail: Text('ju@gmail.com'),
           ),
           InkWell(
             child: Container(
@@ -55,7 +84,34 @@ class CustomDrawer extends StatelessWidget {
                     Text('Termos de uso e privacidade'),
                   ],
                 )),
-            onTap: () {},
+            onTap: () {
+              showModalBottomSheet(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  context: context,
+                  builder: (BuildContext bc) {
+                    return Container(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 12.0, horizontal: 16.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            'Termos de uso e privacidade',
+                            style: TextStyle(
+                                fontSize: 20.0, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(height: 20),
+                          Text(
+                            '  Ainda assim, existem dúvidas a respeito de como a valorização de fatores subjetivos oferece uma interessante oportunidade para verificação da gestão inovadora da qual fazemos parte. Todavia, a estrutura atual da organização faz parte de um processo de gerenciamento do fluxo de informações. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que o aumento do diálogo entre os diferentes setores produtivos desafia a capacidade de equalização do sistema de participação geral. O cuidado em identificar pontos críticos no desenvolvimento contínuo de distintas formas de atuação cumpre um papel essencial na formulação das regras de conduta normativas. Por conseguinte, a consolidação das estruturas facilita a criação dos modos de operação convencionais.',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                        ],
+                      ),
+                    );
+                  });
+            },
           ),
           Divider(),
           SizedBox(height: 10),
