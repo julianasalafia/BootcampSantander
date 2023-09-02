012036641c697940c09740c5db1442d1b5b307ea
julianasalafia <juliana.salafia1@gmail.com>
2023-09-02 11:22:52 -0300

    feat: linear progress indicator

--- 2_modulo/trilhaapp/lib/pages/card_page.dart
+++ 2_modulo/trilhaapp/lib/pages/card_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:trilhaapp/pages/card_detail_page.dart';
+import 'package:trilhaapp/repositories/card_detail_repository.dart';
 
 import '../model/card_detail.dart';
 
@@ -11,12 +12,20 @@ class CardPage extends StatefulWidget {
 }
 
 class _CardPageState extends State<CardPage> {
-  var cardDetail = CardDetail(
-      id: 1,
-      title: 'Meu Card',
-      url: 'https://hermes.digitalinnovation.one/assets/diome/logo.png',
-      text:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+  CardDetail? cardDetail;
+  var cardDetailRepository = CardDetailRepository();
+
+  @override
+  void initState() {
+    super.initState();
+    carregarDados();
+  }
+
+  void carregarDados() async {
+    cardDetail = await cardDetailRepository.get();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -24,64 +33,68 @@ class _CardPageState extends State<CardPage> {
         Container(
           margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           width: double.infinity,
-          child: InkWell(
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => CardDetailPage(
-                            cardDetail: cardDetail,
-                          )));
-            },
-            child: Hero(
-              tag: cardDetail.id,
-              child: Card(
-                elevation: 3,
-                shadowColor: Colors.grey,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 16.0, vertical: 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Image.network(
-                            cardDetail.url,
-                            height: 20,
-                          ),
-                          SizedBox(width: 10),
-                          Text(
-                            cardDetail.title,
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.w700),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        cardDetail.text,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 16.0, fontWeight: FontWeight.normal),
-                      ),
-                      Container(
-                          width: double.infinity,
-                          alignment: Alignment.centerRight,
-                          child: TextButton(
-                              onPressed: () {},
-                              child: Text(
-                                'Ler mais',
-                                style: TextStyle(
-                                  decoration: TextDecoration.underline,
+          child: cardDetail == null
+              ? LinearProgressIndicator()
+              : InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => CardDetailPage(
+                                  cardDetail: cardDetail!,
+                                )));
+                  },
+                  child: Hero(
+                    tag: cardDetail!.id,
+                    child: Card(
+                      elevation: 3,
+                      shadowColor: Colors.grey,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 16.0, vertical: 8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Image.network(
+                                  cardDetail!.url,
+                                  height: 20,
                                 ),
-                              ))),
-                    ],
+                                SizedBox(width: 10),
+                                Text(
+                                  cardDetail!.title,
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w700),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              cardDetail!.text,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.normal),
+                            ),
+                            Container(
+                                width: double.infinity,
+                                alignment: Alignment.centerRight,
+                                child: TextButton(
+                                    onPressed: () {},
+                                    child: Text(
+                                      'Ler mais',
+                                      style: TextStyle(
+                                        decoration: TextDecoration.underline,
+                                      ),
+                                    ))),
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-          ),
         ),
       ],
     );
