0422c28c7da82e50ba20d5fd2e017b4666ad2fe8
julianasalafia <juliana.salafia1@gmail.com>
2023-09-09 00:23:39 -0300

    feat: adicionando sqlite na lista de tarefas

--- 2_modulo/trilhaapp_versao_4/lib/main.dart
+++ 2_modulo/trilhaapp_versao_4/lib/main.dart
@@ -1,37 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:trilhaapp/model/dados_cadastrais_model.dart';
 import 'package:trilhaapp/model/tarefa_hive_model.dart';
 import 'my_app.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
-import 'package:path/path.dart' as path;
-
-Map<int, String> scripts = {
-  1: ''' CREATE TABLE tarefas (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  descricao TEXT,
-  concluido INTEGER
-  );  '''
-};
-
-Future iniciarBancoDeDados() async {
-  var db = await openDatabase(
-    path.join(await getDatabasesPath(), 'database.db'),
-    version: scripts.length,
-    onCreate: (Database db, int version) async {
-      for (var i = 1; i < scripts.length; i++) {
-        await db.execute(scripts[i]!);
-      }
-    },
-    onUpgrade: (Database db, int oldVersion, int newVersion) async {
-      for (var i = oldVersion + 1; i < scripts.length; i++) {
-        await db.execute(scripts[i]!);
-      }
-    },
-  );
-  return db;
-}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
---
+++ 2_modulo/trilhaapp_versao_4/lib/pages/tarefa_page/tarefa_sqlite_page.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:trilhaapp/model/tarefa_hive_model.dart';
+import 'package:trilhaapp/model/tarefa_sqlite_model.dart';
+import 'package:trilhaapp/repositories/tarefa_hive_repository.dart';
+
+import '../../repositories/sqlite/tarefa_sqlite_repository.dart';
+
+class TarefaSQLitePage extends StatefulWidget {
+  const TarefaSQLitePage({super.key});
+
+  @override
+  State<TarefaSQLitePage> createState() => _TarefaSQLitePageState();
+}
+
+class _TarefaSQLitePageState extends State<TarefaSQLitePage> {
+  TarefaSQLiteRepository tarefaSQLiteRepository = TarefaSQLiteRepository();
+  var _tarefas = const <TarefaSQLiteModel>[];
+  var descricaoController = TextEditingController();
+  var apenasNaoConcluidos = false;
+
+  @override
+  void initState() {
+    super.initState();
+    obterTarefas();
+  }
+
+  void obterTarefas() async {
+    _tarefas = await tarefaSQLiteRepository.obterDados(apenasNaoConcluidos);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            descricaoController.text = '';
+            showDialog(
+                context: context,
+                builder: (BuildContext bc) {
+                  return AlertDialog(
+                    title: Text('Adicionar tarefa'),
+                    content: TextField(
+                      controller: descricaoController,
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text('Cancelar'),
+                      ),
+                      TextButton(
+                        onPressed: () async {
+                          await tarefaSQLiteRepository.salvar(TarefaSQLiteModel(
+                              0, descricaoController.text, false));
+                          Navigator.pop(context);
+                          obterTarefas();
+                          setState(() {});
+                        },
+                        child: Text('Salvar'),
+                      ),
+                    ],
+                  );
+                });
+          },
+          child: Icon(Icons.add),
+        ),
+        body: Container(
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Apenas não concluído',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Switch(
+                        value: apenasNaoConcluidos,
+                        onChanged: (bool value) {
+                          apenasNaoConcluidos = value;
+                          obterTarefas();
+                        }),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: _tarefas.length,
+                    itemBuilder: (BuildContext bc, int index) {
+                      var tarefa = _tarefas[index];
+                      return Dismissible(
+                        onDismissed: (DismissDirection dismissDirection) async {
+                          tarefaSQLiteRepository.remover(tarefa.id);
+                          obterTarefas();
+                        },
+                        key: Key(tarefa.descricao),
+                        child: ListTile(
+                          title: Text(tarefa.descricao),
+                          trailing: Switch(
+                            onChanged: (bool value) async {
+                              tarefa.concluido = value;
+                              tarefaSQLiteRepository.atualizar(tarefa);
+                              obterTarefas();
+                            },
+                            value: tarefa.concluido,
+                          ),
+                        ),
+                      );
+                    }),
+              ),
+            ],
+          ),
+        ));
+  }
+}
