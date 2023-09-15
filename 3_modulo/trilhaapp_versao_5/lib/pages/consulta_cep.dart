d74e5f7c6d00e9eda97aafffeda43adcc89c077e
julianasalafia <juliana.salafia1@gmail.com>
2023-09-15 12:45:32 -0300

    feat: conhecendo o ViaCEP e criando a model

--- 3_modulo/trilhaapp_versao_5/lib/pages/consulta_cep.dart
+++ 3_modulo/trilhaapp_versao_5/lib/pages/consulta_cep.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:trilhaapp/model/via_cep_model.dart';
 
 class ConsultaCEP extends StatefulWidget {
   const ConsultaCEP({super.key});
@@ -13,6 +16,7 @@ class _ConsultaCEPState extends State<ConsultaCEP> {
   String endereco = '';
   String cidade = '';
   String estado = '';
+  bool loading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -29,20 +33,32 @@ class _ConsultaCEPState extends State<ConsultaCEP> {
             TextField(
               controller: cepController,
               keyboardType: TextInputType.number,
-              onChanged: (String value) {
+              onChanged: (String value) async {
                 if (value != null && value.trim().length == 8) {
                   var cep = value.replaceAll(RegExp(r'[^0-9]'), '');
                   print(cep);
                   if (cep.length == 8) {
-                    cidade = "Cidade";
-                    estado = "Estado";
-                    endereco = "Endereco";
+                    setState(() {
+                      loading = true;
+                    });
+                    var response = await http
+                        .get(Uri.parse('https://viacep.com.br/ws/$cep/json/'));
+                    print(response.statusCode);
+                    print(response.body);
+                    var json = jsonDecode(response.body);
+                    var viaCepModel = ViaCEPModel.fromJson(json);
+                    print(viaCepModel);
+                    cidade = viaCepModel.localidade ?? '';
+                    estado = viaCepModel.uf ?? '';
+                    endereco = viaCepModel.logradouro ?? '';
                   } else {
                     cidade = '';
                     estado = '';
                     endereco = '';
                   }
-                  setState(() {});
+                  setState(() {
+                    loading = false;
+                  });
                 }
               },
             ),
@@ -55,6 +71,7 @@ class _ConsultaCEPState extends State<ConsultaCEP> {
               '$cidade - $estado',
               style: TextStyle(fontSize: 22),
             ),
+            if (loading) CircularProgressIndicator(),
           ],
         ),
       ),
