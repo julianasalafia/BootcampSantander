5b0564fc395647879c0b25603eb180b2588ae855
julianasalafia <juliana.salafia1@gmail.com>
2023-09-16 12:31:04 -0300

    feat: exibindo dados de comentarios

--- 3_modulo/trilhaapp_versao_6/lib/pages/posts_page.dart
+++ 3_modulo/trilhaapp_versao_6/lib/pages/posts_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trilhaapp/pages/comments_page.dart';
 import '../model/post_model.dart';
 import '../repositories/posts_repository.dart';
 
@@ -35,28 +36,38 @@ class _PostsPageState extends State<PostsPage> {
           itemCount: posts.length,
           itemBuilder: (_, index) {
             var post = posts[index];
-            return Container(
-              margin:
-                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
-              child: Card(
-                child: Container(
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        post.title,
-                        style: TextStyle(
-                            fontSize: 16.0, fontWeight: FontWeight.w700),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        post.body,
-                        style: TextStyle(
-                            fontSize: 14.0, fontWeight: FontWeight.w400),
-                      ),
-                    ],
+            return InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => CommentsPage(postId: post.id),
+                  ),
+                );
+              },
+              child: Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
+                child: Card(
+                  child: Container(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          post.title,
+                          style: TextStyle(
+                              fontSize: 16.0, fontWeight: FontWeight.w700),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          post.body,
+                          style: TextStyle(
+                              fontSize: 14.0, fontWeight: FontWeight.w400),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
