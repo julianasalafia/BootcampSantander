1fa3f78b6eb9a5ddaa8b487fee9d6cd6f8995c35
julianasalafia <juliana.salafia1@gmail.com>
2023-09-17 13:09:25 -0300

    feat: criando uma dio customizada para a jsonplaceholder

--- 3_modulo/trilhaapp_versao_7/lib/repositories/comments/impl/comments_dio_repository.dart
+++ 3_modulo/trilhaapp_versao_7/lib/repositories/comments/impl/comments_dio_repository.dart
@@ -1,13 +1,14 @@
-import 'package:dio/dio.dart';
 import 'package:trilhaapp/model/comment_model.dart';
 import 'package:trilhaapp/repositories/comments/impl/comments_repository.dart';
+
+import '../../jsonplaceholder_custom_dio.dart';
 
 class CommentsDioRepository implements CommentsRepository {
   @override
   Future<List<CommentModel>> retornaComentarios(int postId) async {
-    var dio = Dio();
-    var response = await dio
-        .get('https://jsonplaceholder.typicode.com/posts/$postId/comments');
+    var jsonPlaceHolderCustomDio = JsonPlaceHolderCustomDio();
+    var response =
+        await jsonPlaceHolderCustomDio.dio.get('/posts/$postId/comments');
     return (response.data as List)
         .map((e) => CommentModel.fromJson(e))
         .toList();
--- 3_modulo/trilhaapp_versao_7/lib/repositories/posts/impl/posts_dio_repository.dart
+++ 3_modulo/trilhaapp_versao_7/lib/repositories/posts/impl/posts_dio_repository.dart
@@ -1,14 +1,13 @@
-import 'dart:convert';
-
 import 'package:trilhaapp/model/post_model.dart';
 import 'package:trilhaapp/repositories/posts/posts_repository.dart';
-import 'package:dio/dio.dart';
+
+import '../../jsonplaceholder_custom_dio.dart';
 
 class PostsDioRepository implements PostsRepository {
   @override
   Future<List<PostModel>> getPosts() async {
-    var dio = Dio();
-    var response = await dio.get('https://jsonplaceholder.typicode.com/posts');
+    var jsonPlaceHolderCustomDio = JsonPlaceHolderCustomDio();
+    var response = await jsonPlaceHolderCustomDio.dio.get('/posts');
     if (response.statusCode == 200) {
       return (response.data as List).map((e) => PostModel.fromJson(e)).toList();
     } else {
