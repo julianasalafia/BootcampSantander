9cc5c800491fd626d401f72fd1017a0250043936
julianasalafia <juliana.salafia1@gmail.com>
2023-09-21 13:41:10 -0300

    feat: criando interceptor e dio customizada

--- 3_modulo/trilhaapp_versao_8/lib/repositories/back4app/tarefas_back4app_repository.dart
+++ 3_modulo/trilhaapp_versao_8/lib/repositories/back4app/tarefas_back4app_repository.dart
@@ -1,18 +1,10 @@
-import 'package:dio/dio.dart';
+import 'package:trilhaapp/repositories/back4app/back4app_custom_dio.dart';
 import 'package:trilhaapp/repositories/back4app/tarefas_back4app_model.dart';
 
 class TarefasBack4AppRepository {
-  var _dio = Dio();
+  final _customDio = Back4AppCustomDio();
 
-  TarefasBack4AppRepository() {
-    _dio = Dio();
-    _dio.options.headers['X-Parse-Application-Id'] =
-        'qlyTA0QwIhtAIN4TZLYPD9bkpb7wrxc7vl1U1r8A';
-    _dio.options.headers['X-Parse-REST-API-Key'] =
-        'jXewZxAMPzeU8RLglBX9mfWdRZm4pGWzvLXmyTjX';
-    _dio.options.headers['Content-Type'] = 'application/json';
-    _dio.options.baseUrl = 'https://parseapi.back4app.com/classes';
-  }
+  TarefasBack4AppRepository();
 
   Future<TarefasBack4AppModel> obterTarefas(bool naoConcluidos) async {
     var url = '/Tarefas';
@@ -20,13 +12,14 @@ class TarefasBack4AppRepository {
     if (naoConcluidos) {
       url = '$url?where={\"concluido\":false}';
     }
-    var result = await _dio.get(url);
+    var result = await _customDio.dio.get(url);
     return TarefasBack4AppModel.fromJson(result.data);
   }
 
   Future<void> criar(TarefaBack4AppModel tarefaBack4AppModel) async {
     try {
-      await _dio.post('/Tarefas', data: tarefaBack4AppModel.toJsonEndpoint());
+      await _customDio.dio
+          .post('/Tarefas', data: tarefaBack4AppModel.toJsonEndpoint());
     } catch (e) {
       rethrow;
     }
@@ -34,7 +27,7 @@ class TarefasBack4AppRepository {
 
   Future<void> atualizar(TarefaBack4AppModel tarefaBack4AppModel) async {
     try {
-      await _dio.put('/Tarefas/${tarefaBack4AppModel.objectId}',
+      await _customDio.dio.put('/Tarefas/${tarefaBack4AppModel.objectId}',
           data: tarefaBack4AppModel.toJsonEndpoint());
     } catch (e) {
       rethrow;
@@ -43,7 +36,7 @@ class TarefasBack4AppRepository {
 
   Future<void> remover(String objectId) async {
     try {
-      await _dio.delete('/Tarefas/$objectId');
+      await _customDio.dio.delete('/Tarefas/$objectId');
     } catch (e) {
       rethrow;
     }
