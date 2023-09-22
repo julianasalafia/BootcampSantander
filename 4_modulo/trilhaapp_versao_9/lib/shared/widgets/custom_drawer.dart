7b54fedf8d8cf110804d1d3432aa5cbb498fa5e3
julianasalafia <juliana.salafia1@gmail.com>
2023-09-22 11:22:23 -0300

    feat: instalando o font awesome

--- 4_modulo/trilhaapp_versao_9/lib/shared/widgets/custom_drawer.dart
+++ 4_modulo/trilhaapp_versao_9/lib/shared/widgets/custom_drawer.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:trilhaapp/configuracoes/configuracoes_hive_page.dart';
-import 'package:trilhaapp/pages/characters/characters_page.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:trilhaapp/pages/login_page.dart';
-import 'package:trilhaapp/pages/numeros_aleatorios/numeros_aleatorios_hive.dart';
-import 'package:trilhaapp/pages/posts_page.dart';
-import 'package:trilhaapp/pages/tarefa_page/tarefa_http_page.dart';
-import 'package:trilhaapp/repositories/back4app/tarefas_back4app_repository.dart';
 import '../../dados_cadastrais/dados_cadastrais_hive.dart';
 
 class CustomDrawer extends StatelessWidget {
@@ -60,11 +55,15 @@ class CustomDrawer extends StatelessWidget {
                 padding:
                     const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
                 width: double.infinity,
-                child: Row(
+                child: const Row(
                   children: [
-                    Icon(Icons.person),
+                    FaIcon(
+                      FontAwesomeIcons.user,
+                      color: Colors.pink,
+                      size: 25,
+                    ),
                     SizedBox(width: 10),
-                    Text('Dados cadastrais'),
+                    Text('Opção 1'),
                   ],
                 )),
             onTap: () {
@@ -77,161 +76,6 @@ class CustomDrawer extends StatelessWidget {
           ),
           Divider(),
           SizedBox(height: 10),
-          InkWell(
-            child: Container(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                width: double.infinity,
-                child: Row(
-                  children: [
-                    Icon(Icons.info),
-                    SizedBox(width: 10),
-                    Text('Termos de uso e privacidade'),
-                  ],
-                )),
-            onTap: () {
-              showModalBottomSheet(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  context: context,
-                  builder: (BuildContext bc) {
-                    return Container(
-                      padding: EdgeInsets.symmetric(
-                          vertical: 12.0, horizontal: 16.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            'Termos de uso e privacidade',
-                            style: TextStyle(
-                                fontSize: 20.0, fontWeight: FontWeight.bold),
-                          ),
-                          SizedBox(height: 20),
-                          Text(
-                            '  Ainda assim, existem dúvidas a respeito de como a valorização de fatores subjetivos oferece uma interessante oportunidade para verificação da gestão inovadora da qual fazemos parte. Todavia, a estrutura atual da organização faz parte de um processo de gerenciamento do fluxo de informações. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que o aumento do diálogo entre os diferentes setores produtivos desafia a capacidade de equalização do sistema de participação geral. O cuidado em identificar pontos críticos no desenvolvimento contínuo de distintas formas de atuação cumpre um papel essencial na formulação das regras de conduta normativas. Por conseguinte, a consolidação das estruturas facilita a criação dos modos de operação convencionais.',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(fontSize: 16.0),
-                          ),
-                        ],
-                      ),
-                    );
-                  });
-            },
-          ),
-          Divider(),
-          SizedBox(height: 10),
-          InkWell(
-            child: Container(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                width: double.infinity,
-                child: Row(
-                  children: [
-                    Icon(Icons.gpp_maybe_rounded),
-                    SizedBox(width: 10),
-                    Text('Configurações Hive'),
-                  ],
-                )),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (BuildContext bc) =>
-                          const ConfiguracoesHivePage()));
-            },
-          ),
-          Divider(),
-          SizedBox(height: 10),
-          InkWell(
-            child: Container(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                width: double.infinity,
-                child: Row(
-                  children: [
-                    Icon(Icons.numbers),
-                    SizedBox(width: 10),
-                    Text('Gerador de números'),
-                  ],
-                )),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (BuildContext bc) =>
-                          const NumerosAleatoriosHivePage()));
-            },
-          ),
-          Divider(),
-          SizedBox(height: 10),
-          InkWell(
-            child: Container(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                width: double.infinity,
-                child: Row(
-                  children: [
-                    Icon(Icons.post_add),
-                    SizedBox(width: 10),
-                    Text('Posts'),
-                  ],
-                )),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (BuildContext bc) => const PostsPage()));
-            },
-          ),
-          Divider(),
-          SizedBox(height: 10),
-          InkWell(
-            child: Container(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                width: double.infinity,
-                child: Row(
-                  children: [
-                    Icon(Icons.help),
-                    SizedBox(width: 10),
-                    Text('Herois'),
-                  ],
-                )),
-            onTap: () async {
-              Navigator.pop(context);
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (BuildContext bc) => const CharactersPage()));
-            },
-          ),
-          Divider(),
-          SizedBox(height: 10),
-          InkWell(
-            child: Container(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                width: double.infinity,
-                child: const Row(
-                  children: [
-                    Icon(Icons.help),
-                    SizedBox(width: 10),
-                    Text('Tarefas HTTP'),
-                  ],
-                )),
-            onTap: () async {
-              Navigator.pop(context);
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (BuildContext bc) => const TarefaHttpPage()));
-            },
-          ),
-          const Divider(),
-          const SizedBox(height: 10),
           InkWell(
             child: Container(
                 padding:
