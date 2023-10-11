b778a9d307cf71d857ee380322931602a0dbc84a
julianasalafia <juliana.salafia1@gmail.com>
2023-10-11 12:36:53 -0300

    feat: adiciona modular

--- 5_Lab_Projects/lista_de_contatos/lib/main.dart
+++ 5_Lab_Projects/lista_de_contatos/lib/main.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:lista_de_contatos/modules/app_module.dart';
+import 'package:lista_de_contatos/modules/app_widget.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  runApp(ModularApp(module: AppModule(), child: const AppWidget()));
 }
