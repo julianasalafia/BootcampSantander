9a88bf7f034e05f0034baa3ff6554996456dcb02
julianasalafia <juliana.salafia1@gmail.com>
2023-10-15 20:48:37 -0300

    feat: update contact form model

--- 5_Lab_Projects/lista_de_contatos/lib/models/new_contact_form_model.dart
+++ 5_Lab_Projects/lista_de_contatos/lib/models/new_contact_form_model.dart
@@ -1,15 +1,68 @@
 class NewContactFormModel {
-  final String photo;
-  final String name;
-  final String surname;
-  final String phone;
-  final String email;
+  List<Contact> contacts = [];
 
-  NewContactFormModel({
-    required this.photo,
-    required this.name,
-    required this.surname,
-    required this.phone,
-    required this.email,
+  NewContactFormModel(this.contacts);
+
+  NewContactFormModel.fromJson(Map<String, dynamic> json) {
+    if (json['results'] != null) {
+      contacts = <Contact>[];
+      json['results'].forEach((v) {
+        contacts.add(Contact.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['results'] = contacts.map((v) => v.toJson()).toList();
+    return data;
+  }
+}
+
+class Contact {
+  final String objectId;
+  String? photo;
+  String? name;
+  String? surname;
+  String? phone;
+  String? email;
+  String? createdAt;
+  String? updatedAt;
+
+  Contact({
+    required this.objectId,
+    this.photo,
+    this.name,
+    this.surname,
+    this.phone,
+    this.email,
+    this.createdAt,
+    this.updatedAt,
   });
+
+  factory Contact.fromJson(Map<String, dynamic> json) {
+    return Contact(
+      objectId: json['objectId'],
+      photo: json['photo'] ?? '',
+      name: json['name'] ?? '',
+      surname: json['surname'] ?? '',
+      phone: json['phone'] ?? '',
+      email: json['email'] ?? '',
+      createdAt: json['createdAt'] ?? '',
+      updatedAt: json['updatedAt'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'objectId': objectId,
+      'photo': photo,
+      'name': name,
+      'surname': surname,
+      'phone': phone,
+      'email': email,
+      'createdAt': createdAt,
+      'updatedAt': updatedAt,
+    };
+  }
 }
