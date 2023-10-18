b0adc1442fc6af1e543b3e2b682e88b1b3859c2e
julianasalafia <juliana.salafia1@gmail.com>
2023-10-18 13:06:00 -0300

    feat: update constants file

--- 5_Lab_Projects/lista_de_contatos/lib/helper/constants.dart
+++ 5_Lab_Projects/lista_de_contatos/lib/helper/constants.dart
@@ -11,3 +11,10 @@ const saveButton = 'Save';
 
 // background
 const backgroundImage = 'assets/empty_background.png';
+
+// var
+const name = 'Name';
+const surname = 'Surname';
+const phone = 'Phone';
+const email = 'Email';
+const addImageMessage = 'Adicionar imagem';
