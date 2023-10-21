ddb76252fb37cb46809e60f7b95302a3d32cf28b
julianasalafia <juliana.salafia1@gmail.com>
2023-10-21 00:40:53 -0300

    feat: update constants file

--- 5_Lab_Projects/lista_de_contatos/lib/helper/constants.dart
+++ 5_Lab_Projects/lista_de_contatos/lib/helper/constants.dart
@@ -8,13 +8,31 @@ const newContactPage = '/newcontact';
 
 // buttons
 const saveButton = 'Save';
+const cancelButton = 'Cancel';
+const confirmButton = 'Ok';
 
 // background
 const backgroundImage = 'assets/empty_background.png';
 
 // var
-const name = 'Name';
+const name = 'Name*';
 const surname = 'Surname';
-const phone = 'Phone';
+const phone = 'Phone*';
 const email = 'Email';
-const addImageMessage = 'Adicionar imagem';
+const addImageMessage = 'Add image';
+
+// dialogs
+const warningTitleDialog = 'Warning!';
+const warningMessage =
+    'Fill in the required fields to complete the registration';
+
+const warningDeleteMessage = 'Are you sure you want to delete this contact?';
+
+const successfulTitleDialog = 'Success!';
+const successfulMessageDialog = 'Contact successfully registered!';
+
+const successfulDeleteTitleDialog = 'Done!';
+const successfulDeleteMessageDialog = 'Contact successfully deleted!';
+
+const tryAgainMessage = 'Try again';
+const voidMessage = '';
